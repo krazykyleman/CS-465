4b033fa6964f07494a041855eee82987ac8e5520
krazykyleman <Kyle.wucik@gmail.com>
2023-10-04 17:08:48 -0400

    fixed fixed

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ var logger = require('morgan');
 
 var hbs = require('hbs');
 
-require('./app_api/database/db')
-
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
-var apiRouter = require('./app_api/routes/index')
 
 
 var app = express();
@@ -31,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
-app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
