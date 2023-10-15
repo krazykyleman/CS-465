0dec59cc356f3a9b7afb63aaaaf34b7352a6c28c
krazykyleman <Kyle.wucik@gmail.com>
2023-10-15 14:45:38 -0400

    Update app-routing.module.ts

--- app_admin/src/app/app-routing.module.ts
+++ app_admin/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { AddTripComponent } from './add-trip/add-trip.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: TripListingComponent, pathMatch: 'full' },
+  { path: 'add-trip', component: AddTripComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
